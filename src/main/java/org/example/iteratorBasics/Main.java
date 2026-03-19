204ea956846e75b87d85218d708e15b89e458840
Yahi-0001 <yah18462@gmail.com>
2026-03-18 18:29:47 -0600

    Actualización: Estructura de paquetes iteratorBasics finalizada

--- src/main/java/org/example/iteratorBasics/Main.java
+++ src/main/java/org/example/iteratorBasics/Main.java
@@ -6,19 +6,29 @@ package org.example.iteratorBasics;
 public class Main {
     public static void main(String[] args) {
 
-        // Agregamos la variable filmVector
+        // Agregamos la variable filmVector creamos un array con 10 espacios
         Film[] filmVector = new Film[10];
 
+        // Se crea una colección, entregando el array
         var myCol = new MyMarvelCollection(filmVector);
+
+        // Con el metodo add se agrega cada pelicula en el array
         myCol.add (new Film ("Ironman", 2008));
         myCol.add (new Film ("Captain America", 2010));
         myCol.add (new Film ("The Avengers", 2012));
 
+
+        //Se crea un objeto apuntando al inicio en -1
         var iterator = myCol.iterator ();
+
+        //Preguntamos si hay algo adelante
         while (iterator.hasNext ()) {
+            //se mueve, imprime información y espera la instruccion
             System.out.println (iterator.next ());
         }
 
+        // para este proceso agregamos a la clase implements Iterable
+        //Preguntamos si es una coleccion y entregamos un objeto e imprimimos los datos
         for (var film: myCol) {
             System.out.println (film);
         }
--- src/main/java/org/example/iteratorBasics/MyMarvelCollection.java
+++ src/main/java/org/example/iteratorBasics/MyMarvelCollection.java
@@ -7,14 +7,19 @@ import java.util.Iterator;
 
 public class MyMarvelCollection implements Iterable<Film> {
 
+    // creamos un array fijo  y un count para la posicion
     private Film[] films;
     private int count;
 
+    // Constructor para contener los objetos
     public MyMarvelCollection(Film[] filmVector){
         this.films = filmVector;
         this.count = 0;
     }
 
+    /* El metodo add recibe la pelicula, verifiamos que el array no este lleno
+    * lo coloca en la posicion actual manteniendo orden en la insercion
+    * */
     public void add(Film film){
         if(count < films.length){
             films[count] = film;
@@ -22,6 +27,10 @@ public class MyMarvelCollection implements Iterable<Film> {
         }
     }
 
+    /* el metodo iterator crea un objeto para recorrer el array
+    * con hasNext verificamos no pasarnos del numero de peliculas
+    * con Next damos la pelicula actual y avanzamos a la siguiente posición
+    * */
     @Override
     public Iterator<Film> iterator(){
         return new Iterator<Film>() {
